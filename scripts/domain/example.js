a86b66bc91f42ca19bfcd7309a8e8ddbce8226fe
Taylor Leach <taylorleach@hotmail.com>
2018-08-25 21:35:38 -0700

    tightened all margins and padding vertically on mobile. added in fade transition for content on scroll.

---
+++ scripts/domain/example.js
@@ -0,0 +1,22 @@
+var below_hero_section = document.querySelector('#below-hero-wrapper');
+var details_section = document.querySelector('.details-section');
+
+window.onscroll = () => {
+
+  if (isScrolledIntoView(below_hero_section)) below_hero_section.style.opacity = '1';
+  else below_hero_section.style.opacity = '0';
+
+  if (isScrolledIntoView(details_section)) details_section.style.opacity = '1';
+  else details_section.style.opacity = '0';
+
+
+
+
+}
+
+function isScrolledIntoView(element) {
+  var rect = element.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+  return elemTop < window.innerHeight && elemBottom >= 0;
+}
